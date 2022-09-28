92c3de545d0acccd3de7ff80e55b2edba126f842
NPMI User <NPMIUser@npr.co.jp>
2022-09-28 11:08:18 +0700

    Arduino

---
+++ src/main.cpp
@@ -0,0 +1,79 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <DMD2.h>
+#include "font/SystemFont5x7.h"
+#include "font/Arial_Black_16.h"
+
+const int WIDTH = 1; // jumlah panel led matrik yang digunakan
+const uint8_t *FONT = Arial_Black_16;
+
+const byte numChars = 16;
+char receivedChars[numChars];   // an array to store the received data
+
+boolean newData = false;
+
+
+SoftDMD dmd(WIDTH,1); 
+DMD_TextBox box(dmd);
+
+
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("<Arduino is ready>");
+    dmd.setBrightness(255);
+    dmd.selectFont(FONT);
+    dmd.begin();
+}
+
+
+void recvWithEndMarker() {
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+    
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (rc != endMarker) {
+            receivedChars[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars) {
+                ndx = numChars - 1;
+            }
+        }
+
+
+        else {
+            receivedChars[ndx] = '\0'; // terminate the string
+            ndx = 0;
+            newData = true;
+        }
+    }
+}
+
+
+void sensor1(){
+     if (newData == true) {
+        Serial.print("This just in ... ");
+        Serial.println(receivedChars);
+         
+        newData = false;
+    }
+    
+    const char *next = receivedChars;
+        while(*next) {
+        box.print(*next);
+        delay(1000);
+        next++;
+         }
+         
+        //box.print(receivedChars);
+}
+
+
+void loop() {
+    recvWithEndMarker();
+    sensor1();
+    
+}
